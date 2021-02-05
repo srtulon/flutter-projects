d323ba685b6d5ee5a3151ac7e05da58e8276189f
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-02-05 23:03:55 +0600

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('hello'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('body'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    ),
   ));
 }
