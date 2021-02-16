f1bdc16418a063af439243fcecb68f4bebb9d5b3
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-02-16 20:11:39 +0600

    Update main.dart

--- idcard/lib/main.dart
+++ idcard/lib/main.dart
@@ -8,7 +8,12 @@ void main() {
 }
 
 
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
