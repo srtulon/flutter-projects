e2cc12a1c0a9113e99df4b47daf6f93d136ea49e
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-02-19 20:32:03 +0600

    Update main.dart

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,5 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    home: QuoteList()
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes=[
+   'The world isn’t perfect. But it’s there for us, doing the best it can….that’s what makes it so damn beautiful',
+    'How can you move forward when you keep regretting the past?',
+    'To know sorrow is not terrifying. What is terrifying is to know you can’t go back to happiness you could have',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote){
+          return Text(quote);
+        }).toList(),
+      ),
+    );
+  }
 }
