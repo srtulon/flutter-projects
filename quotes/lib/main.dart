3222aea1ee4c18be8889a0831810151470130ac8
Sadid Rafsun Tulon <srtulon6@gmail.com>
2021-02-22 19:38:33 +0600

    Update main.dart

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -20,6 +20,35 @@ class _QuoteListState extends State<QuoteList> {
   Quote(text:'To know sorrow is not terrifying. What is terrifying is to know you can’t go back to happiness you could have',author:'3'),
   ];
 
+  Widget quoteTemplate (quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[700],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +59,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.deepPurpleAccent,
       ),
       body: Column(
-        children: quotes.map((quote) =>Text('${quote.text}-${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
